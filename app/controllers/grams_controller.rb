13587293fc34450b792f56809f203dea8caf7ebb
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-07-01 16:06:14 -0700

    Add Destroy gram functionality

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -30,6 +30,17 @@ class GramsController < ApplicationController
     
   end
 
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+
+     return render_not_found if @gram.blank?
+      @gram.destroy
+      if @gram.valid?
+      redirect_to root_path
+      
+    end
+    
+  end
 
   def update
     @gram = Gram.find_by_id(params[:id])
